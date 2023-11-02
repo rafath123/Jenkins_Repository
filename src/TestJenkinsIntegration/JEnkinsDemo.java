c22b827d22cef17ec105056c525afc3801ed26bf
Rafath <rrafathmurtujahokrani@gmail.com>
2023-11-02 17:12:27 +0000

    !st jenkins commit

---
+++ src/TestJenkinsIntegration/JEnkinsDemo.java
@@ -0,0 +1,11 @@
+package TestJenkinsIntegration;
+
+public class JEnkinsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Jenkins");
+		System.out.println("Lets start integrating with git using eclipse");
+	}
+
+}
